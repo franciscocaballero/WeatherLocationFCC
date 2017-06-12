def6024f7dea4d124e33c27f51ea0cc7d3b096e3
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-06-11 22:22:08 -0400

    adding some styling

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var lat;
 var long;
 var fTemp;
 var cTemp;
+
+
 $.getJSON('http://ip-api.com/json', function(data2){
   lat = data2.lat;
   long = data2.lon;
-  console.log(long);
 
 
-// var api = 'http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=5d291dcae7f9d1a344a6ea02391feef7'
 var api = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=5d291dcae7f9d1a344a6ea02391feef7'
 
 $.getJSON(api, function(data){
@@ -21,6 +21,25 @@ $.getJSON(api, function(data){
 
   fTemp = (kelvin)*(9/5)-459.67;
   cTemp = kelvin-273;
+  var tempSwap = true;
+
+  $('#city').html(city);
+  $('#weatherType').html(weatherType);
+  $('#fTemp').html(fTemp);
+  $('#fTemp').click(function(){
+
+    if(tempSwap === false){
+      $('#fTemp').html(cTemp);
+      tempSwap = true;
+    }else{
+      $('#fTemp').html(fTemp);
+      tempSwap = false;
+    }
+  })
+
+
+
+  $('#windSpeed').html(windSpeed);
 })
 
 });
